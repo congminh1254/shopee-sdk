dd795871a605fe5eab2d56c605c93735d3797152
Minh Nguyen Cong <mcong@box.com>
2025-04-15 00:48:58 +0200

    Generate URL

--- src/storage/custom-token-storage.ts
+++ src/storage/custom-token-storage.ts
@@ -5,19 +5,23 @@ import path from 'path';
 
 export class CustomTokenStorage implements TokenStorage {
   private readonly tokenPath: string;
-
+  private readonly defaultTokenPath: string;
   constructor(shopId?: number) {
     // create a folder in the root of the project called .token
     const tokenDir = path.join(process.cwd(), '.token');
     if (!fs.existsSync(tokenDir)) {
       fs.mkdirSync(tokenDir, { recursive: true });
     }
+    this.defaultTokenPath = path.join(tokenDir, 'default.json');
     this.tokenPath = path.join(tokenDir, `${shopId ?? 'default'}.json`);
   }
 
   public async store(token: AccessToken): Promise<void> {
     try {
       fs.writeFileSync(this.tokenPath, JSON.stringify(token, null, 2));
+      if (this.defaultTokenPath !== this.tokenPath && !fs.existsSync(this.defaultTokenPath)) {
+        fs.writeFileSync(this.defaultTokenPath, JSON.stringify(token, null, 2));
+      }
     } catch (error) {
       throw new Error(`Failed to store token: ${error instanceof Error ? error.message : 'Unknown error'}`);
     }
